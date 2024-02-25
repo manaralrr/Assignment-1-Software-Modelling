ce04ff4914cc3efc236324c157445f54d8206486
manaralrr <160352954+manaralrr@users.noreply.github.com>
2024-02-25 13:01:37 +0530

    Create flight.py

---
+++ flight.py
@@ -0,0 +1,11 @@
+class Flight:
+
+    def __init__(self,airline,flight_no,origin,destination,dept_time,arrival_time,gate,dept_date):
+        self.airline = airline
+        self.flight_no = flight_no
+        self.origin = origin
+        self.destination = destination
+        self.dept_time = dept_time
+        self.arrival_time = arrival_time
+        self.gate = gate
+        self.dept_date = dept_date
