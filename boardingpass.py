01dffcef84ce2de2b81d260d584a1cb5570ffa24
manaralrr <160352954+manaralrr@users.noreply.github.com>
2024-02-25 13:23:48 +0530

    Update boardingpass.py

--- boardingpass.py
+++ boardingpass.py
@@ -3,3 +3,15 @@ class BoardingPass:
     def __init__(self,passenger,flight):
         self.passenger = passenger
         self.flight = flight
+
+    def verify_booking(self):
+        '''The function check the booking status and other related information to verify booking.'''
+        pass
+
+    def issue_boarding_pass(self):
+        '''The function makes the boarding pass with all details'''
+        pass
+
+    def update_flight_status(self):
+        '''The function cheks the flight status and update the same if any changes are required.'''
+        pass
